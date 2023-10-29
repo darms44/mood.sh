b237c6712d9f6e6fa0b5ae89bc17400112d504af
dustin arms <darms44@gmail.com>
2023-10-29 12:21:43 -0700

    added 'good' option

--- mood.sh
+++ mood.sh
@@ -17,7 +17,7 @@ while true; do
 	alright) echo "alright alright alright"; break;;
 	angry|mad) echo "I'm sorry that you are upset. Take some deep breaths and don't over react."; break;;
 	great|wonderful) echo "Awesome $name!! I'm so happy for you!"; break;;
-	ok) echo "okie dokie artichokie"; break;;
+	ok|good) echo "okie dokie artichokie"; break;;
 	fuck*you|fuck*off) echo "Really? FUCK YOU! Buddy."; break;;
 	*) echo "I'm sorry, I don't understand that, please enter 'happy, sad, tired etc...'.";;
 	esac
@@ -27,9 +27,9 @@ while true; do
 	read -p "What is the time of day?" time
 	time=${time,,}
     case "$time" in
-	morning)  
+	morning)
 		case "$mood" in  
-		happy|alright|ok) echo "It is a beautiful morning, $name!"; break;;
+		happy|alright|ok|good) echo "It is a beautiful morning, $name!"; break;;
 		sad) echo "It's still early, I hope it gets better for you $name :)"; break;;
 		tired) echo "I think its coffee time!"; break;;
 		angry|mad) echo "Try to do something for yourself $name, self care is vital"; break;;
@@ -43,7 +43,7 @@ case "$time" in
 		happy|great|wonderful) echo "What an exciting day!"; break;;
 		sad) echo "Brighter days are just around the corner $name!";break;;
 		tired) echo "Maybe a nap will help?"; break;;
-		alright|ok) echo "Is it lunch time yet?"; break;;
+		alright|ok|good) echo "Is it lunch time yet?"; break;;
 		angry|mad) echo "Take a break $name, it might help :)"; break;;
                 fuck*you|fuck*off) echo "Good for you, bitch."; break;;
 		esac
@@ -54,7 +54,7 @@ case "$time" in
 		happy) echo "I'm glad that you had a good day $name :)"; break;;
 		sad) echo "Well $name, at least the day is over, Tomorrow is a new day!"; break;;
 		tired) echo "Then go to bed!"; break;;
-		alright|ok) echo "Have a good evening $name"; break;;
+		alright|ok|good) echo "Have a good evening $name"; break;;
                 angry|mad) echo "Get some rest $name, tomorrow can be a better day."; break;;
 		great|wonderful) echo "That's awesome $name :)"; break;;
                 fuck*you|fuck*off) echo "I hope you have nightmares."; break;;
@@ -68,10 +68,11 @@ sleep .3s
 	read -p "Would you like to try again?" redo
 	redo=${redo,,}
 	case $mood in
-		fuck*you|fuck*off) echo "Cunt"; break;;
+		fuck*you|fuck*off) echo "Cunt"; sleep 5s; break;;
 	esac
 	case $redo in
 		yes) ;;
 		no) echo "Ok, thanks for talking with me today $name!"; break;;
 	esac
+sleep 2s
 done
